dc9f8983b4ffaeeb63a1cbabf0464aaadf778548
okihusni <okihusni@gmail.com>
2021-04-13 16:12:46 +0700

    [REST API] success respond PUT /todos/:id, PATCH /todos:id, DELETE /todos/:id

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -28,9 +28,12 @@ class TodosController {
   static getTodosId(req, res) {
     const { id } = req.params
 
-    Todo.findAll({ where: { id: +id }, attributes: { exclude: ['createdAt', 'updatedAt'] } })
+    Todo.findByPk(+id, { attributes: { exclude: ['createdAt', 'updatedAt'] } })
     .then(todo => {
-      res.status(200).json(todo)
+      if(todo) {
+        res.status(200).json(todo)
+      }
+      else res.status(404).json({err: "not found"})
     })
     .catch(err => {
       res.status (500).json(err)
@@ -41,18 +44,63 @@ class TodosController {
     const { id } = req.params
     const { title, description, status, due_date } = req.body
 
-    Todo.update({ where: {id: +id}, title, description, status, due_date})
+    Todo.findByPk(+id)
     .then(todo => {
-      console.log(todo);
+      if(todo) {
+        todo.title = title
+        todo.description = description
+        todo.status = status
+        todo.due_date = due_date
+
+        return todo.save()
+      }
+      else {
+        res.status(404).json({error: "not found"})
+      }
+    })
+    .then(todo => {
+      res.status(200).json({ title: todo.title, description: todo.description, status: todo.status, due_date: todo.due_date })
+    })
+    .catch(err => {
+      res.status(500).json(err)
     })
   }
 
   static patchTodosId(req, res) {
-    res.send('edit some element in todo by id')
+    const { id } = req.params
+    const { status } = req.body
+
+    Todo.findByPk(+id)
+    .then(todo => {
+      if(todo) {
+        todo.status = status
+
+        return todo.save()
+      }
+      else res.status(404).json({error: "not found"})
+    })
+    .then(todo => {
+      res.status(200).json({ title: todo.title, description: todo.description, status: todo.status, due_date: todo.due_date })
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
   }
 
   static deleteTodosId(req, res) {
-    res.send('delete todo by id')
+    const { id } = req.params
+
+    Todo.findByPk(+id)
+    .then(result => {
+      if(result) return result.destroy()
+      else res.status(404).json({ error: "not found" })
+    })
+    .then(result => {
+      res.status(200).json({message: "todo success to delete"})
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
   }
 
 }
